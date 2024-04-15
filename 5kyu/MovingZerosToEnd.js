a71d059330f19af01e3679f24106d4af7065c7f9
Megan Allas <allasmct@gmail.com>
2024-04-15 12:16:50 -0400

    refine

--- 5kyu/MovingZerosToEnd.js
+++ 5kyu/MovingZerosToEnd.js
@@ -1,18 +1,18 @@
+// 1n
 function moveZeros(arr) {
-    let count = 0;
-  
-    for (let i = arr.length - 1; i >= 0; i--) {
-      if (arr[i] === 0) {
-        arr.splice(arr.indexOf(0), 1);
-        count++;
-      }
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === 0) {
+      arr.splice(arr.indexOf(0), 1);
+      arr.push(0);
     }
-  
-    if (count > 0) {
-      for (let k = 1; k <= count; k++) {
-        arr.push(0);
-      }
-    }
-    
-    return arr;
   }
+  
+  return arr;
+}
+
+// 2n
+// function moveZeros(arr) {
+//   var filtedList = arr.filter((num) => num !== 0);
+//   var zeroList = arr.filter((num) => num !== 0);
+//   return filtedList.concat(zeroList);
+// }
