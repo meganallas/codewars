eb8467059c1ebed08965e690b0b242a960c1e9e6
Megan Allas <allasmct@gmail.com>
2024-05-04 18:47:22 -0400

    6kyu - find the parity outlier

---
+++ 6kyu/FindTheParityOutlier.js
@@ -0,0 +1,15 @@
+function findOutlier(integers) {
+  const even = [];
+  const odd = [];
+
+  for (let i = 0; i < integers.length; i++) {
+    if (integers[i] % 2 === 0) {
+      even.push(integers[i]);
+    } else {
+      odd.push(integers[i]);
+    }
+  }
+
+  if (even.length > odd.length) return odd[0];
+  else return even[0];
+}
